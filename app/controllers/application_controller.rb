c9f56508fbaa338abe32b7cfa11556c643f91844
Bryce Eadie <bryce.eadie@gmail.com>
2018-11-15 09:45:54 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,6 +14,12 @@ class App < Sinatra::Base
       @hero1_name = params[:team][:members][0][:name]
       @hero1_power = params[:team][:members][0][:power]
       @hero1_bio = params[:team][:members][0][:bio]
+      @hero2_name = params[:team][:members][1][:name]
+      @hero2_power = params[:team][:members][1][:power]
+      @hero2_bio = params[:team][:members][1][:bio]
+      @hero3_name = params[:team][:members][2][:name]
+      @hero3_power = params[:team][:members][2][:power]
+      @hero3_bio = params[:team][:members][2][:bio]
       binding.pry
       erb :team
     end
